91d071b1d8ede74bb92ed179f686daf4cefaf56b
Antonio Liccardo <tuxmal@tiscali.it>
2011-02-05 20:43:51 +0100

    Aggiunti gli stub dei test

---
+++ spec/to_do_spec.rb
@@ -0,0 +1,15 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'to_do'
+
+describe ToDo do
+  before(:each) do
+    @to_do = ToDo.new
+  end
+
+  it "should desc" do
+    # TODO
+  end
+end
+
