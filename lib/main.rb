22bcd0457410e4e73849f09cc9b6658bf43c45b7
Antonio Liccardo <tuxmal@tiscali.it>
2011-02-07 20:32:51 +0100

    Some code from http://techbase.kde.org/Development/Tutorials/Plasma/Ruby/SimplePasteApplet to set the base plasmoid code!

--- lib/main.rb
+++ lib/main.rb
@@ -1,32 +1,71 @@
 #required libraries to rely on.
 #require 'rubygems'
-
+require 'plasma_applet'
 require 'yaml'
 #require 'enumerator'
-
-#require 'file'
 
 #We need ToDo tasks
 require 'to_do'
 require 'task'
 
-to_do = ToDo::ToDo.new
+module RubyRubyDo
+  class Main < PlasmaScripting::Applet
 
-a_task = ToDo::Task.new 'compra il latte', 1, Time.now + 1
-to_do.add a_task
+    slots :addText
 
-a_task = ToDo::Task.new 'Telefonare!', 2, Time.now + 3
-to_do.add a_task
+    def init
+      set_minimum_size 150, 150
 
-a_task = ToDo::Task.new 'Garage', nil, Time.now + 1
-to_do.add a_task
+      @layout = Qt::GraphicsLinearLayout.new Qt::Vertical, self
+      self.layout = @layout
 
-a_task = ToDo::Task.new 'bollette', 5
-to_do.add a_task
+      @label = Plasma::Label.new self
+      @label.text = 'This plasmoid will copy the text you enter below to the clipboard.'
+      @layout.add_item @label
 
-to_do.each {|t| puts t.to_yaml}
-to_do.save
+      @line_edit = Plasma::LineEdit.new self
 
-to_do = ToDo::ToDo.new
-to_do.load
-to_do.each {|t| puts t.to_yaml}
+      begin
+        @line_edit.clear_button_shown = true # not supported in early plasma versions
+      rescue
+        nil # but that doesn't matter
+      end
+
+      @layout.add_item @line_edit
+
+      @button = Plasma::PushButton.new self
+      @button.text = 'Copy to clipboard'
+      @layout.add_item @button
+
+      Qt::Object.connect( @button, SIGNAL(:clicked), self, SLOT(:addText) )
+      Qt::Object.connect( @line_edit, SIGNAL(:returnPressed), self, SLOT(:addText) )
+    end
+
+    def addText
+      Qt::Application.clipboard.text = @line_edit.text
+      @line_edit.text = ""
+    end
+
+  end
+end
+
+#to_do = ToDo::ToDo.new
+#
+#a_task = ToDo::Task.new 'compra il latte', 1, Time.now + 1
+#to_do.add a_task
+#
+#a_task = ToDo::Task.new 'Telefonare!', 2, Time.now + 3
+#to_do.add a_task
+#
+#a_task = ToDo::Task.new 'Garage', nil, Time.now + 1
+#to_do.add a_task
+#
+#a_task = ToDo::Task.new 'bollette', 5
+#to_do.add a_task
+#
+#to_do.each {|t| puts t.to_yaml}
+#to_do.save
+#
+#to_do = ToDo::ToDo.new
+#to_do.load
+#to_do.each {|t| puts t.to_yaml}
