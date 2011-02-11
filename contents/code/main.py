d515b1a4a040cc5f72fa08e741517e7a73346250
Antonio Liccardo <antonio.liccardo@unina2.it>
2011-02-11 16:20:15 +0100

    Now we can propose a treeview in our plasmoid. Added two file for an example in python.

---
+++ contents/code/main.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+#   Copyright (C) 2009 Andrew Stromme
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License version 2,
+#   or (at your option) any later version, as published by the Free
+#   Software Foundation
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the
+#   Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyKDE4.kdecore import i18n
+from PyKDE4.kdeui import *
+from PyKDE4.plasma import Plasma
+from PyKDE4 import plasmascript
+import dbus
+
+class TreeViewExample(plasmascript.Applet):
+    def __init__(self,parent,args=None):
+        plasmascript.Applet.__init__(self,parent)
+
+    def init(self):
+        self.setHasConfigurationInterface(False)
+        self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
+
+        self.theme = Plasma.Svg(self)
+        self.theme.setImagePath("widgets/background")
+        self.setBackgroundHints(Plasma.Applet.DefaultBackground)
+
+        self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
+
+        self.label = Plasma.Label(self.applet)
+        self.label.setText(i18n("TreeView Example:"))
+        self.layout.addItem(self.label)
+
+        self.stringlist = QStringList()
+        for i in range(1,10):
+          self.stringlist.append("Item " + str(i))
+
+        self.model = QStringListModel(self.applet)
+        self.model.setStringList(self.stringlist)
+
+        treeview = Plasma.TreeView(self.applet)
+        treeview.setModel(self.model)
+        self.layout.addItem(treeview)
+
+        self.lineedit = Plasma.LineEdit(self.applet)
+        self.lineedit.nativeWidget().setClearButtonShown(True)
+        self.lineedit.nativeWidget().setClickMessage(i18n("Add a new string..."))
+        QObject.connect(self.lineedit, SIGNAL("returnPressed()"), self.addString)
+        self.layout.addItem(self.lineedit)
+
+        self.setLayout(self.layout)
+        self.resize(250,400)
+
+    def addString(self):
+        self.stringlist.append(self.lineedit.text())
+        self.lineedit.nativeWidget().clear()
+        self.model.setStringList(self.stringlist)
+
+def CreateApplet(parent):
+    return TreeViewExample(parent)
--- lib/main.rb
+++ lib/main.rb
@@ -16,36 +16,66 @@ module RubyRubyDo
     slots :addText
 
     def init
-      set_minimum_size 150, 150
-
+      set_minimum_size 250, 400
+      self.has_configuration_interface = false
+      #self.setAspectRatioMode= Plasma::IgnoreAspectRatio
+      # py: self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
       @layout = Qt::GraphicsLinearLayout.new Qt::Vertical, self
       self.layout = @layout
 
+        #self.theme = Plasma::Svg(self)
+        #self.theme.setImagePath("widgets/background")
+        #self.setBackgroundHints(Plasma::Applet.DefaultBackground)
+
       @label = Plasma::Label.new self
-      @label.text = 'This plasmoid will copy the text you enter below to the clipboard.'
-      @layout.add_item @label
+      @label.text = 'TreeView Example:'
+      @layout.addItem @label
 
-      @line_edit = Plasma::LineEdit.new self
+      @stringlist = [] #Qt::StringList.new
+      (1..10).each { |i| @stringlist << "Item #{i}" }
 
+      @model = Qt::StringListModel.new self
+      @model.StringList= @stringlist
+
+      @treeview = Plasma::TreeView.new self
+      @treeview.Model = @model
+      @layout.add_item @treeview
+
+      @lineedit = Plasma::LineEdit.new self
       begin
         @line_edit.clear_button_shown = true # not supported in early plasma versions
       rescue
         nil # but that doesn't matter
       end
+      #@lineedit.click_message = 'Add a new string...'
+      Qt::Object.connect(@lineedit,  SIGNAL(:returnPressed), self, SLOT(:addText) )
+      @layout.addItem @lineedit
 
-      @layout.add_item @line_edit
+        self.setLayout(self.layout)
+        self.resize(250,400)
 
-      @button = Plasma::PushButton.new self
-      @button.text = 'Copy to clipboard'
-      @layout.add_item @button
 
-      Qt::Object.connect( @button, SIGNAL(:clicked), self, SLOT(:addText) )
-      Qt::Object.connect( @line_edit, SIGNAL(:returnPressed), self, SLOT(:addText) )
+
+      #@label = Plasma::Label.new self
+      #@label.text = 'This plasmoid will copy the text you enter below to the clipboard.'
+      #@layout.add_item @label
+
+
+      #@layout.add_item @line_edit
+
+      #@button = Plasma::PushButton.new self
+      #@button.text = 'Copy to clipboard'
+      #@layout.add_item @button
+
+      #Qt::Object.connect( @button, SIGNAL(:clicked), self, SLOT(:addText) )
+      #Qt::Object.connect( @line_edit, SIGNAL(:returnPressed), self, SLOT(:addText) )
     end
 
     def addText
-      Qt::Application.clipboard.text = @line_edit.text
-      @line_edit.text = ""
+      Qt::Application.clipboard.text = @lineedit.text
+      @stringlist << @lineedit.text
+      @model.string_list = @stringlist
+      @lineedit.text = ""
     end
 
   end
