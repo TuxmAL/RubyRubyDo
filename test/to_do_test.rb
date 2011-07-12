389d41a954b3311a8d5cbfac0d19dce8b5232aa4
Antonio Liccardo <antonio.liccardo@unina2.it>
2011-07-12 10:45:18 +0200

    Modified tests to add new kind of tasks.

--- test/to_do_test.rb
+++ test/to_do_test.rb
@@ -18,28 +18,31 @@ class ToDoTest < Test::Unit::TestCase
     @task1 = ToDo::Task.new 'compra il latte', 1, @today
     @task2 = ToDo::Task.new 'Telefonare!', 2, @tomorrow
     @task3 = ToDo::Task.new 'Garage', 3
+    @task4 = ToDo::Task.new 'Andare a pesca', 2, Date.today - 9 
+    @task5 = ToDo::Task.new 'Andare a pesca', 2, Date.today - 3
+    @task5.done
+    @todo_list << @task1 << @task2 << @task3 << @task4 << @task5
   end
 
-  must 'append multiple task at once' do
-    @todo_list << @task1 << @task2 << @task3
-    assert_equal(3, @todo_list.length)
+  must 'append multiple task at once' do   
+    assert_equal(5, @todo_list.length)
   end
 
   must 'delete a task by task object' do
-    @todo_list << @task1 << @task2 << @task3
+    before = @todo_list.length
     @todo_list.delete(@task2)
-    assert_equal(2, @todo_list.length)
+    assert_equal(before - 1, @todo_list.length)
     assert_block("Expected #{@task2.inspect} to be deleted") do
       @todo_list.first == @task1 &&
-      @todo_list.last == @task3
+      @todo_list.last == @task5
     end
   end
 
   must 'insert a task at specified index' do
-    @todo_list << @task1 << @task2 << @task3
+     before = @todo_list.length
     task_x = ToDo::Task.new 'must be the second!', 5
     @todo_list.insert(1, task_x)
-    assert_equal(4, @todo_list.length)
+    assert_equal(before + 1, @todo_list.length)
     assert_block("Expected #{task_x.inspect} to be 2nd place in the list") do
       @todo_list[0] == @task1 &&
       @todo_list[1] == task_x
