95f2ee5b04c44d754a99d85b1d2566db1a4a5038
Antonio Liccardo <tuxmal@tiscali.it>
2011-10-09 16:25:27 +0200

    Implemented a better methodo to calculate size hints.

--- lib/ruby_ruby_do/to_do_model_item.rb
+++ lib/ruby_ruby_do/to_do_model_item.rb
@@ -25,13 +25,12 @@ module RubyRubyDo
     attr_reader :children
 
     @@font = nil
-    @@size_hint = nil
     #@@icons = {} #{:collapsed => nil, :expanded => nil}
 
     def self.font=(font)
       @@font = font
       fi = Qt::FontInfo.new @@font
-      @@size_hint = Qt::Size.new(30, fi.pixel_size + 2)
+      @@font_metrics = Qt::FontMetrics.new(font)
       @@font_to_do = font.point_size_f
       @@font_category = @@font_to_do + TODO_FONT_SIZE_DIFFERENCE
     end
@@ -141,8 +140,8 @@ module RubyRubyDo
         case role
             #when Qt::BackgroundRole
             #  return @brush_for_data
-	when Qt::SizeHintRole
-	    return Qt::Variant.new(@@size_hint)
+          when Qt::SizeHintRole
+            ret_val = @@font_metrics.size(Qt::TextSingleLine, task.description)+ Qt::Size.new(0, 2) if column == 2
           when Qt::CheckStateRole
             if column == 0
                 ret_val = ((task.done?)? Qt::Checked: Qt::Unchecked).to_i
