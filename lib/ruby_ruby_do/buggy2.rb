df676fc41ff772ad836c74f1811e27a7ad505cce
Antonio Liccardo <tuxmal@tiscali.it>
2013-01-30 22:06:04 +0100

    Files to prove a nasty bug on qtruby with QAbstractModel self implemented.

---
+++ lib/ruby_ruby_do/buggy2.rb
@@ -0,0 +1,63 @@
+# -*- encoding: UTF-8 -*-
+require 'Qt4'
+
+class MyModel < Qt::AbstractItemModel
+
+  def initialize(parent = nil)
+    super parent
+  end
+
+  def data(index, role)
+    return Qt::Variant.new if (not index.valid?)
+    case role
+      #### needed both for bug action!!!
+      #when Qt::StatusTipRole, Qt::ToolTipRole
+      #  ret_val = 'StatusTip, ToolTip'
+      #  return Qt::Variant.new(ret_val)
+      when Qt::DisplayRole
+        return Qt::Variant.new("datum")
+      else
+        return Qt::Variant.new
+    end
+  end
+
+  def rowCount(index)
+    1
+  end
+
+  def columnCount(index)
+    3
+  end
+
+  def flags(index)
+    return Qt::NoItemFlags
+  end
+
+end
+
+Qt::Application.new(ARGV) do
+  Qt::Widget.new do
+    self.window_title = 'test bug'
+    self.layout = Qt::VBoxLayout.new do
+      treeview = Qt::TreeView.new do #self
+        mdl = MyModel.new(Qt::Application.instance)
+        # but also the following lines can be used
+        # mdl = MyModel.new self
+        #mdl = MyModel.new
+
+        ### needed for bug action!
+        self.style_sheet = 'QTreeView::branch {background: palette(base) ;border-image: none;} QTreeView::item:!has-children:has-siblings:!adjoins-item {background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #E1E1E1, stop: 0.4 #DDDDDD, stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3)}'
+
+        self.model = mdl
+      end
+      add_widget treeview
+
+      button_quit = Qt::PushButton.new('Quit') do
+        connect(SIGNAL :clicked) { Qt::Application.instance.quit }
+      end
+      add_widget(button_quit, 0, Qt::AlignRight)
+    end
+    self.show
+  end
+  self.exec
+end
