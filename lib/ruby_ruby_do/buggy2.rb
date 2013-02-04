8f9b0abcf8dc7db4e625d7d344f946a4a724c424
Antonio Liccardo <antonio.liccardo@unina2.it>
2013-02-04 11:12:06 +0100

    Now works with minimal model.

--- lib/ruby_ruby_do/buggy2.rb
+++ lib/ruby_ruby_do/buggy2.rb
@@ -2,11 +2,18 @@
 require 'Qt4'
 
 class MyModel < Qt::AbstractItemModel
-
-  def initialize(parent = nil)
-    super parent
+#  def initialize(parent = nil)
+#    super parent
+#  end
+  def index(row, column = 0, parent = Qt::ModelIndex.new)
+    Qt::ModelIndex.new if not parent.valid?
+    createIndex(row, column, Qt::ModelIndex.new)
   end
-
+  
+  def parent(index)
+    Qt::ModelIndex.new
+  end
+  
   def data(index, role)
     return Qt::Variant.new if (not index.valid?)
     case role
@@ -22,7 +29,7 @@ class MyModel < Qt::AbstractItemModel
   end
 
   def rowCount(index)
-    1
+    5
   end
 
   def columnCount(index)
@@ -32,7 +39,6 @@ class MyModel < Qt::AbstractItemModel
   def flags(index)
     return Qt::NoItemFlags
   end
-
 end
 
 Qt::Application.new(ARGV) do
@@ -40,10 +46,11 @@ Qt::Application.new(ARGV) do
     self.window_title = 'test bug'
     self.layout = Qt::VBoxLayout.new do
       treeview = Qt::TreeView.new do #self
-        mdl = MyModel.new(Qt::Application.instance)
+         #mdl = Qt::DirModel.new
+       mdl = MyModel.new(Qt::Application.instance)
         # but also the following lines can be used
         # mdl = MyModel.new self
-        #mdl = MyModel.new
+#        mdl = MyModel.new
 
         ### needed for bug action!
         self.style_sheet = 'QTreeView::branch {background: palette(base) ;border-image: none;} QTreeView::item:!has-children:has-siblings:!adjoins-item {background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #E1E1E1, stop: 0.4 #DDDDDD, stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3)}'
